2c930d263a92609df0ca4e886c92c13f5e38fc74
BobW7 <1186717311@qq.com>
2023-11-23 18:09:37 +0800

    引入Eureka注册中心，将UserService服务模块注册

--- user-service/src/main/java/com/bob/user/UserApplication.java
+++ user-service/src/main/java/com/bob/user/UserApplication.java
@@ -3,10 +3,12 @@ package com.bob.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.bob.user.mapper")
+@EnableDiscoveryClient //Eureka客户端发现功能，这个服务将被发现
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
