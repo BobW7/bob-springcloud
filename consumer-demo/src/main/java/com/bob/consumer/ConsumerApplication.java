de869d1ddfdd60e69ec876ea0178b84bffcb532e
BobW7 <1186717311@qq.com>
2023-11-24 14:58:10 +0800

    引入Eureka注册中心，将Consumer服务模块注册

--- consumer-demo/src/main/java/com/bob/consumer/ConsumerApplication.java
+++ consumer-demo/src/main/java/com/bob/consumer/ConsumerApplication.java
@@ -2,17 +2,21 @@ package com.bob.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient //开启Eureka客户端
+@ComponentScan("com.bob")
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
--- consumer-demo/src/main/java/com/bob/consumer/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/bob/consumer/controller/ConsumerController.java
@@ -1,12 +1,17 @@
 package com.bob.consumer.controller;
 
 import com.bob.consumer.pojo.User;
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -15,10 +20,16 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
     public User query(@PathVariable Long id) {
         //通过restTemplate给提供服务的模块发送请求，并且将响应（JSON）反序列化为本模块的pojo
         String url = "http://localhost:9001/user/" + id;
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("user-service");
+        ServiceInstance serviceInstance = serviceInstances.get(0);
+        url = "http://"+serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
         return restTemplate.getForObject(url, User.class);
     }
 }
