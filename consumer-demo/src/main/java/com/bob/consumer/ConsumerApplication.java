b6adb530cb7a387695dd366d4e8a811f6f93d09b
BobW7 <1186717311@qq.com>
2023-11-24 16:53:51 +0800

    开启Ribbon负载均衡

--- consumer-demo/src/main/java/com/bob/consumer/ConsumerApplication.java
+++ consumer-demo/src/main/java/com/bob/consumer/ConsumerApplication.java
@@ -3,6 +3,7 @@ package com.bob.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ public class ConsumerApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- consumer-demo/src/main/java/com/bob/consumer/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/bob/consumer/controller/ConsumerController.java
@@ -1,17 +1,13 @@
 package com.bob.consumer.controller;
 
 import com.bob.consumer.pojo.User;
-import com.netflix.appinfo.InstanceInfo;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -25,11 +21,12 @@ public class ConsumerController {
 
     @GetMapping("/{id}")
     public User query(@PathVariable Long id) {
-        //通过restTemplate给提供服务的模块发送请求，并且将响应（JSON）反序列化为本模块的pojo
+        /*//通过restTemplate给提供服务的模块发送请求，并且将响应（JSON）反序列化为本模块的pojo
         String url = "http://localhost:9001/user/" + id;
         List<ServiceInstance> serviceInstances = discoveryClient.getInstances("user-service");
         ServiceInstance serviceInstance = serviceInstances.get(0);
-        url = "http://"+serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
+        url = "http://"+serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;*/
+        String url = "http://user-service/user" + id;
         return restTemplate.getForObject(url, User.class);
     }
 }
